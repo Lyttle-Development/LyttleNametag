017b6806646ef3d0c168b7052b8039b3da25c769
stualyttle <stualyttle@users.noreply.github.com>
2025-05-31 23:08:15 +0200

    use stable version

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ repositories {
 dependencies {
     compileOnly(libs.io.papermc.paper.paper.api)
     compileOnly("me.clip:placeholderapi:2.11.6")
-    implementation("com.lyttledev:lyttleutils:1.1.0-BETA.005")
+    implementation("com.lyttledev:lyttleutils:1.1.0")
 }
 
 group = "com.lyttledev"
