aa38b295a0b0dbf1424032db19280e666fd5cfe6
stualyttle <stualyttle@users.noreply.github.com>
2025-10-04 10:43:21 +0200

    Update build.gradle.kts: upgrade PacketEvents dependency to version 2.9.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ repositories {
 dependencies {
     compileOnly("io.papermc.paper:paper-api:" + (property("paperVersion") as String) + "-R0.1-SNAPSHOT")
     compileOnly("me.clip:placeholderapi:2.11.6")
-    implementation("com.github.retrooper:packetevents-spigot:2.9.4")
+    implementation("com.github.retrooper:packetevents-spigot:2.9.5")
     implementation("com.lyttledev:lyttleutils:1.2.0")
 }
 
